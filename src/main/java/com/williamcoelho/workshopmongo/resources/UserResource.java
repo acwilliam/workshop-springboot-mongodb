575dc58068540ae8d2b9d249e265676bacfeabed
William Carvalho <thelordl2@hotmail.com>
2021-07-08 21:08:38 -0300

    atualizacao de usuario com PUT

--- src/main/java/com/williamcoelho/workshopmongo/resources/UserResource.java
+++ src/main/java/com/williamcoelho/workshopmongo/resources/UserResource.java
@@ -51,4 +51,12 @@ public class UserResource {
 			return ResponseEntity.noContent().build();
 		}
 		
+		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+			User obj = service.fromDTO(objDto);
+			obj.setId(id);
+			obj = service.update(obj);
+			return ResponseEntity.noContent().build();
+		}
+		
 }
