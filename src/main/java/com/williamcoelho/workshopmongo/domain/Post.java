3c4ba95a68857840d737b4f29f536a833d00cb42
William Carvalho <thelordl2@hotmail.com>
2021-07-11 13:16:57 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/williamcoelho/workshopmongo/domain/Post.java
+++ src/main/java/com/williamcoelho/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.williamcoelho.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.williamcoelho.workshopmongo.dto.AuthorDTO;
+import com.williamcoelho.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable {
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
