2457efdde8f0398f6034a0541652b11c346194ca
William Carvalho <thelordl2@hotmail.com>
2021-07-11 12:35:31 -0300

    Referenciando os posts do usuario

--- src/main/java/com/williamcoelho/workshopmongo/config/Instantiantion.java
+++ src/main/java/com/williamcoelho/workshopmongo/config/Instantiantion.java
@@ -39,10 +39,13 @@ public class Instantiantion implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(alex));
-		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia!", "Acordei feliz hoje!", new AuthorDTO(bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo. Abraços!",new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 		
 	}
